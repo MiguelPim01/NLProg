28e92c2c8c4f4dc0a03ea8b4394f5ccac5ae284f
Miguel <miguelmip02@gmail.com>
2023-02-03 14:54:33 -0300

    indices.h parcialmente comentado

--- headers/indices.h
+++ headers/indices.h
@@ -8,20 +8,28 @@
 
 typedef struct indices Indices;
 
+// Realiza a leitura de todos os documentos e armazena na struct indice o indice de palavras e documentos
 Indices * CarregaIndices(FILE *f, Indices *i, char *caminhoAux);
 
+// Salva os indices em um arquivo binario
 void SalvaIndices(Indices *i, FILE *f);
 
+// Realiza a leitura da struct indices de um arquivo binario
 Indices * CarregaIndicesBin(Indices *i, FILE *f);
 
+// Libera indices da memória
 void LiberaIndices(Indices *i);
 
+// Retorna ponteiro para indice de palavras
 IndicePalavras * RetornaIndicePalavras(Indices *i);
 
+// Retorna ponteiro para indice de documentos
 IndiceDocs * RetornaIndiceDocs(Indices *i);
 
+// Gera o relatorio da palavra
 void GeraRelatorioPalavra(Indices *i, char *palavra);
 
+// Gera o relatorio de documentos
 void GeraRelatorioDocs(Indices *i, IndiceDocs *Rdocs);
 
 void GeraMatrizConfusao(Indices *indices, char **classesVerdadeiras, char **classesDeduzidas, FILE *f);
